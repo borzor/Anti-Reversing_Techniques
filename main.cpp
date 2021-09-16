22688cba4d10427d3dc6efae486d3b0d6bf0b437
borzor <borzor@mail.ru>
2021-09-16 14:33:10 -0700

    changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <sys/utsname.h>
 #include <unistd.h>
-#include <csignal>
+#include <cstring>
+#include <sys/ptrace.h>
+#include <wait.h>
 
-std::string generateKey(const std::string &login, const std::string &password) {
-    std::string key;
+#define password_size 32
+
+char* calc_addr(char* p_addr){
+    return p_addr + 0x400000;
+}
+
+void generateKey(const std::string &login, const std::string &password, std::string &key){
+    char* label_address = 0;
+    asm volatile(
+        "jmp unaligned\n"
+        ".short 0xe8\n"
+        "unaligned:");
+    label_address = calc_addr(((char*)&&return_here) - 0x400000);
+    asm volatile(
+        "push %0\n"
+        "ret\n"
+        ".string \"\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\""
+        :
+        : "g"(label_address));
+    return_here:
+
+    std::cout<<login<<"\n";
     uint32_t tmp1,tmp2;
     struct utsname buffer;
     if (uname(&buffer) < 0) {
@@ -19,7 +41,11 @@ std::string generateKey(const std::string &login, const std::string &password) {
             key.push_back(password[i]);
         }
     }
-    std::cout<<key<<'\n';
+    while(key.size() < password_size){
+        key+=key;
+    }
+    key.erase(key.begin()+32,key.end());
+
     for(size_t i = 0; i < key.size()/2; i++){
         key[i] ^=  buffer.sysname[i % strlen(buffer.sysname)];
     }
@@ -29,21 +55,8 @@ std::string generateKey(const std::string &login, const std::string &password) {
     for(size_t i = 0; i < key.size(); i++){
         tmp1 = key[i] << std::stoi(&buffer.release[0])%key.size();
         tmp2 = key[i] >> (key.size() -  std::stoi(&buffer.release[0]) % key.size());
-        key[i] = 31 + (tmp1 | tmp2) % 95;
+        key[i] = 33 + (tmp1 | tmp2) % 95;
     }
-    std::cout<<key<<'\n';
-    return key;
-}
-
-int main() {
-    std::string login="login", password="password", key, checkKey;
-    key = generateKey(login, password);
-    //std::cin>>checkKey;
-    if(!strcmp(key.c_str(),checkKey.c_str())){
-        std::cout<<"congratulation\n";
-    }
-
-    return 0;
 }
 
 void isDebuggerAttached() {
@@ -68,3 +81,57 @@ void isDebuggerAttached() {
     }
     fclose(proc_status);
 }
+int main() {
+    std::string login, password, key, checkKey;
+    std::cout<<"Enter login\n";
+    std::cin>>login;
+    std::cout<<"Enter password\n";
+    std::cin>>password;
+    int fork_pid = fork();
+    if (fork_pid == 0){
+        if (ptrace(PTRACE_ATTACH, getppid(), NULL, NULL) != 0){
+            exit(EXIT_FAILURE);
+        }
+        ptrace(PTRACE_SETOPTIONS, getppid(), NULL, PTRACE_O_TRACEFORK);// PTRACE_O_EXITKILL
+        // restart the parent so it can keep processing like normal
+        int status = 0;
+        wait(&status);
+        ptrace(PTRACE_CONT, getppid(), NULL, NULL);
+        // handle any signals that may come in from tracees
+        while (true)
+        {
+            isDebuggerAttached();
+            int pid = waitpid(-1, &status, WNOHANG);
+            if (pid == 0)
+            {
+                sleep(1);
+                continue;
+            }
+            if (status >> 16 == PTRACE_EVENT_FORK)
+            {
+// follow the fork
+                long newpid = 0;
+                ptrace(PTRACE_GETEVENTMSG, pid, NULL, &newpid);
+                ptrace(PTRACE_ATTACH, newpid, NULL, NULL);
+                ptrace(PTRACE_CONT, newpid, NULL, NULL);
+            }
+            ptrace(PTRACE_CONT, pid, NULL, NULL);
+        }
+
+    }
+    generateKey(login, password,key);
+    printf("your key is %s\n",key.data());
+    printf("Enter key\n");
+    scanf("%s",checkKey.data());
+    if(!strcmp(key.c_str(),checkKey.c_str())){
+        std::cout<<"congratulation\n";
+    }
+
+    return 0;
+}
+
+void __attribute__((constructor)) before_main()
+{
+    isDebuggerAttached();
+}
+
